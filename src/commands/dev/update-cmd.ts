aa2769dad1ffbe5dfe979eb87908f5d5c39d1eef
Donovan Daniels <hewwo@yiff.rocks>
2021-07-06 13:59:40 -0500

    update command formatting

--- src/commands/dev/update-cmd.ts
+++ src/commands/dev/update-cmd.ts
@@ -8,12 +8,15 @@ export default new Command("update")
 	.setParsedFlags("exit")
 	.setExecutor(async function(msg) {
 		try {
-			const out = execSync("git pull");
+			const out = execSync("git pull").toString();
+			const latest = execSync("git log -1").toString().split("\n").map(line => `> ${line}`);
+			latest[3] = "> Commit Message:";
+			if (latest.length === 6) latest.splice(5);
 			const noChanges = out.includes("Already up to date.");
 			const exit = msg.dashedArgs.value.includes("exit");
-			await msg.reply(`Success.${exit ? noChanges ? " No changes were made, not exiting." : " Changes were made, exiting." : ""}\n\`\`\`sh\n${out.toString()}\`\`\``);
+			await msg.reply(`Success.${noChanges ? `No changes were made. ${exit ? " Not exiting." : ""}` : `${exit ? " Exiting in 2 seconds." : " Not exiting."}\n\nCommit Info:\n${latest.join("\n")}`}\n\`\`\`sh\nOutput:\n${out}\`\`\``);
 			await msg.channel.sendTyping();
-			if (exit && !noChanges) setTimeout(() => process.exit(0), 5e3);
+			if (exit && !noChanges) setTimeout(() => process.exit(0), 2e3);
 		} catch (e) {
 			Logger.getLogger("UpdateCommand").error("Update Error", e);
 			return msg.reply("There was an error, check the console.");
