7f907b2dcc82c0cf7d6479173f17967f50363e56
Donovan Daniels <hewwo@yiff.rocks>
2021-07-14 01:50:59 -0500

    Remove useless reference

--- src/commands/nsfw/e621-cmd.ts
+++ src/commands/nsfw/e621-cmd.ts
@@ -1,5 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="../../util/@types/simple-thumbnail.d.ts" />
 import Command from "@cmd/Command";
 import EmbedBuilder from "@util/EmbedBuilder";
 import config from "@config";
