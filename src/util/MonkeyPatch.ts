00d7951fc36bba742c61d112985f8af789a2ad29
Donovan Daniels <hewwo@yiff.rocks>
2021-07-17 19:02:27 -0500

    unnecessary prototype addition

--- src/util/MonkeyPatch.ts
+++ src/util/MonkeyPatch.ts
@@ -329,12 +329,3 @@ Object.defineProperty(Eris, "InteractionCallbackType", {
 		UPDATE_MESSAGE: 7
 	}
 });
-Object.defineProperty(Eris.prototype, "InteractionCallbackType", {
-	value: {
-		PONG: 1,
-		CHANNEL_MESSAGE_WITH_SOURCE: 4,
-		DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE: 5,
-		DEFERRED_UPDATE_MESSAGE: 6,
-		UPDATE_MESSAGE: 7
-	}
-});
