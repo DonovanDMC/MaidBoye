de5e0de1b4f16e61d90b20301bfdcd09a9ef6720
Donovan Daniels <hewwo@yiff.rocks>
2021-07-17 20:44:51 -0500

    "stats" api endpoint

--- src/api/routes/stats.ts
+++ src/api/routes/stats.ts
@@ -1,12 +1,13 @@
 import MaidBoye from "../../main";
 import Route from "../Route";
 import config from "@config";
+import CommandHandler from "@util/cmd/CommandHandler";
 
 export default class StatusRoute extends Route {
 	constructor(client: MaidBoye) {
-		super("/status");
+		super("/stats");
 
-		this.app.get("/", async(req,res) => {
+		this.app.get("/", async(req, res) => {
 			const onlineShards = client.shards.filter(shard => shard.status === "ready");
 			res.status(200).json({
 				success: true,
@@ -20,9 +21,19 @@ export default class StatusRoute extends Route {
 							lastAck: shard.lastHeartbeatAck,
 							lastRecieved: shard.lastHeartbeatReceived,
 							lastSent: shard.lastHeartbeatSent
-						}
+						},
+						guilds: client.guilds.filter(g => g.shard.id === shard.id).length,
+						largeGuilds: client.guilds.filter(g => g.shard.id === shard.id && g.large).length,
+						users: client.guilds.filter(g => g.shard.id === shard.id).reduce((a,b) => a + b.memberCount, 0),
+						channels: client.guilds.filter(g => g.shard.id === shard.id).reduce((a,b) => a + b.channels.size, 0)
 					})),
 					latency: onlineShards.reduce((a,b) => a + b.latency, 0) / onlineShards.length,
+					guilds: client.guilds.size,
+					largeGuilds: client.guilds.filter(g => g.large).length,
+					users: client.guilds.reduce((a,b) => a + b.memberCount, 0),
+					channels: client.guilds.reduce((a,b) => a + b.channels.size, 0),
+					voiceConnections: client.voiceConnections.size,
+					commands: CommandHandler.commands.length,
 					mode: config.beta ? "BETA" : "PROD"
 				}
 			});
