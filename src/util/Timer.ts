d4406e65e908b2da5d2c88644a2416fd61c4f1bf
Donovan Daniels <hewwo@yiff.rocks>
2021-07-19 18:00:29 -0500

    timing

--- src/util/Timer.ts
+++ src/util/Timer.ts
@@ -1,15 +1,12 @@
+import { Time } from "@uwu-codes/utils";
+
 export default class Timer {
 	static getTime() { return process.hrtime.bigint(); }
 	static get start() { return this.getTime.bind(this); }
 	static get end() { return this.getTime.bind(this); }
 
-	static calc(start: bigint, end: bigint, dec: number | undefined, raw: false): { val: number; ns: boolean; };
-	static calc(start: bigint, end: bigint, dec?: number, raw?: true): number;
 	static calc(start: bigint, end: bigint, dec = 3, raw = true) {
 		const v = Number(end - start);
-		return raw === true ? parseFloat((v * 1e-6).toFixed(dec)) : {
-			val: v < 1e6 ? v : parseFloat((v * 1e-6).toFixed(dec)),
-			ns: v < 1e6
-		};
+		return raw === true ? parseFloat((v).toFixed(dec)) : Time.convert(parseFloat((v).toFixed(dec)), "ns", dec);
 	}
 }
