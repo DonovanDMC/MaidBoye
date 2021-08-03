2d72708c5e4312898d17f57206053e4d8d1c19f4
Donovan Daniels <hewwo@yiff.rocks>
2021-08-03 03:32:42 -0500

    write build date to build dir, if exists

--- scripts/build-date.ts
+++ scripts/build-date.ts
@@ -6,3 +6,5 @@ const d = new Date();
 (pkg.buildDate as string | null) = `${d.getMonth().toString().padStart(2, "0")}${(d.getDate() + 1).toString().padStart(2, "0")}${d.getFullYear()}`;
 console.log("New Build Date:", pkg.buildDate === null ? "Unknown" : `${String(pkg.buildDate).slice(0, 2)}/${String(pkg.buildDate).slice(2, 4)}/${String(pkg.buildDate).slice(4, 8)}`);
 fs.writeFileSync(`${__dirname}/../package.json`, JSON.stringify(pkg, undefined, "  "));
+// if built, write to build dir as well
+if (fs.existsSync(`${__dirname}/../build`)) fs.writeFileSync(`${__dirname}/../build/package.json`, JSON.stringify(pkg, undefined, "  "));
