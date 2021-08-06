e489c1c73696893f2712512d61a241f7d2d067b7
Donovan Daniels <hewwo@yiff.rocks>
2021-08-05 19:19:05 -0500

    uinfo nearby joins fix

--- src/commands/information/uinfo-cmd.ts
+++ src/commands/information/uinfo-cmd.ts
@@ -22,9 +22,7 @@ export default new Command("uinfo", "userinfo")
 		// @FIXME banners don't seem to be sent over gateway yet
 		if (user && (user.banner === undefined || user.accentColor === undefined)) user = await this.getUser(user.id, true);
 		if (user === null) return msg.reply("Th-that isn't a valid user..");
-		const member = msg.channel.guild.members.get(user.id);
-		const sortedMembers = Array.from(msg.channel.guild.members.values()).sort((a,b) => (a.joinedAt ?? 0) - (b.joinedAt ?? 0));
-
+		const member = msg.channel.guild.members.get(user.id)!;
 
 		let dRep: { id: string; upvotes: number; downvotes: number; xp: number; rank: string; staff: boolean; } | undefined, infr: { id: string; } & ({ type: "CLEAN";} | { type: "WARN" | "BAN"; reason: string; moderator: string; date: number; }) | undefined;
 		try {
@@ -40,7 +38,7 @@ export default new Command("uinfo", "userinfo")
 					"Authorization": config.apiKeys["discord-rep"]
 				}
 			}).then(v => v.json() as unknown as typeof infr);
-		} catch (e) {
+		} catch {
 			//
 		}
 
@@ -48,16 +46,28 @@ export default new Command("uinfo", "userinfo")
 		if (config.developers.includes(user.id)) badges.push("DEVELOPER");
 		if (badges.length === 0) badges.push("NONE");
 
-		const joinPosition = member && (sortedMembers.indexOf(member) + 1);
-		const joins = [] as Array<Eris.Member>;
-		// I didn't feel like making a function for this, so we do it manually
-		if (member && joinPosition) {
-			if (joinPosition === 1) joins.push(member, ...sortedMembers.slice(1, 5));
-			else if (joinPosition === 2) joins.push(sortedMembers[0], member, ...sortedMembers.slice(2, 5));
-			else if (joinPosition === (sortedMembers.length - 1)) joins.push(...sortedMembers.slice(-5, -2), member, sortedMembers[sortedMembers.length - 1]);
-			else if (joinPosition === (sortedMembers.length)) joins.push(...sortedMembers.slice(-5, -1), member);
-			else joins.push(...sortedMembers.slice(joinPosition - 3, joinPosition - 1), member, ...sortedMembers.slice(joinPosition, joinPosition + 2));
+		// this is legacy code but more importantly, IT WORKS
+		//                                                              I don't know where joined_at would be null
+		const m = Array.from(msg.channel.guild.members.values()).sort((a, b) => a.joinedAt! - b.joinedAt!).map(v => v.id);
+		// eslint-disable-next-line no-inner-declarations
+		function workItOut(n: boolean, amount = 2) {
+			amount++;
+			const k: Array<string> = [];
+			for (let i = 1; i < amount; i++) {
+				const d = n ? m.indexOf(user!.id) - i : m.indexOf(user!.id) + i;
+				if (d < 0 || d > (m.length - 1)) continue;
+				else k.push(m[d]);
+			}
+			return k;
 		}
+
+		let one = workItOut(true).reverse();
+		let two = workItOut(false);
+		if (one.length === 0) two = workItOut(false, 4);
+		else if (one.length === 1) two = workItOut(false, 3);
+		else if (two.length === 0) one = workItOut(false, 4);
+		else if (two.length === 1) one = workItOut(false, 3);
+		const around = [...one, user.id, ...two];
 
 		return msg.reply({
 			embeds: [
@@ -78,7 +88,7 @@ export default new Command("uinfo", "userinfo")
 							`${config.emojis.default.dot} Join Date: ${member.joinedAt === null ? "Unknown" : BotFunctions.formatDiscordTime(member.joinedAt, "long-datetime", true)}`,
 							`${config.emojis.default.dot} Roles: ${member.roles.length === 0 ? "**None**" : member.roles.reduce((a,b) => a + b.length + 4 /* <@&> */, 0) > 1500 ? "**Unable To Display Roles.**" : member.roles.map(r => `<@&${r}>`).join(" ")}`,
 							`${config.emojis.default.dot} Join Info:`,
-							...joins.map(j => `${config.emojis.default.dot} ${j.id === member.id ? `**#${sortedMembers.indexOf(j) + 1} ${j.tag}**` : `#${sortedMembers.indexOf(j) + 1} ${j.tag}`}`)
+							...around.map(a => `${a === user!.id ? `- **[#${m.indexOf(a) + 1}]**` : `- [#${m.indexOf(a) + 1}]`} <@!${a}> (${BotFunctions.formatDiscordTime(msg.channel.guild.members.get(a)!.joinedAt!, "short-datetime", true)})`)
 						],
 						"",
 						`[**DiscordRep**](https://discordrep.com/u/${user.id}):`,
