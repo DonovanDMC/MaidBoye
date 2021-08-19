f5629571b2201e40c8489920c94db06652d9dec1
Donovan Daniels <hewwo@yiff.rocks>
2021-08-19 11:28:45 -0500

    more eris updates

--- src/util/ClientEvent.ts
+++ src/util/ClientEvent.ts
@@ -1,7 +1,7 @@
 import MaidBoye from "../main";
 import Eris from "eris";
 
-export default class ClientEvent<K extends keyof Eris.ClientEvents =  keyof Eris.ClientEvents> {
+export default class ClientEvent<K extends keyof Eris.ClientEvents = keyof Eris.ClientEvents> {
 	name: K;
 	listener: (this: MaidBoye, ...args: Eris.ClientEvents[K]) => void;
 	constructor(event: K, listener: (this: MaidBoye, ...args: Eris.ClientEvents[K]) => void) {
