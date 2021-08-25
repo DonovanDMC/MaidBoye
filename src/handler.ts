e87bb11e2209207cc8de9753272a475eb03bf5d6
Donovan Daniels <hewwo@yiff.rocks>
2021-08-24 18:56:11 -0800

    client changes

--- src/handler.ts
+++ src/handler.ts
@@ -1,12 +1,18 @@
-import { handleRequest as handleInteraction } from "./bot";
+import { handleRequest as handleInteraction } from './bot'
 
 export async function handleRequest(request: Request): Promise<Response> {
-  const { pathname } = new URL(request.url);
+  const { pathname } = new URL(request.url)
   // return new Response(`request method: ${request.method}`)
-  if(request.method === "POST") return handleInteraction(request);
+  if (request.method === 'POST') return handleInteraction(request)
   else {
-    if(pathname === "/invite") return Response.redirect(`https://discord.com/api/oauth2/authorize?client_id=${BOT_ID}&scope=applications.commands`);
-    else if(pathname === "/support") return Response.redirect("https://api.maid.gay/links/support");
-    else return Response.redirect("https://lite.maid.gay/invite");
+    if (pathname === '/invite')
+      return Response.redirect(
+        `https://discord.com/api/oauth2/authorize?client_id=${BOT_ID}&scope=applications.commands&redirect_uri=${encodeURIComponent(
+          REDIRECT_URI,
+        )}`,
+      )
+    else if (pathname === '/support')
+      return Response.redirect('https://api.maid.gay/links/support')
+    else return Response.redirect('https://lite.maid.gay/invite')
   }
 }
