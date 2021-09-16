7613d4423069d8bcd88d37bc7dc7678bc787235e
Donovan Daniels <hewwo@yiff.rocks>
2021-09-15 22:41:32 -0500

    same thing for fursuitbutt command

--- src/commands/nsfw/fursuitbutt.ts
+++ src/commands/nsfw/fursuitbutt.ts
@@ -26,7 +26,7 @@ export default new Command("fursuitbutt")
 				.addURLButton(img.sources[0] || "https://yiff.rest", img.sources.length === 0, undefined, "Source")
 				.addURLButton(img.reportURL, false, undefined, "Report")
 				.addInteractionButton(ComponentHelper.BUTTON_SECONDARY, `fursuitbutt-newimg.${msg.author.id}`, false, undefined, "New Image")
-				.addInteractionButton(ComponentHelper.BUTTON_DANGER, `general-exit.${msg.author.id}`, false, undefined, "Exit")
+				.addInteractionButton(ComponentHelper.BUTTON_DANGER, `general-exit-2.${msg.author.id}`, false, undefined, "Exit")
 				.toJSON()
 		});
 	});
