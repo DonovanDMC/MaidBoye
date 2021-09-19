5af84afaa9f7971c463b87919cf467a456bbe10f
Donovan Daniels <hewwo@yiff.rocks>
2021-09-19 10:06:46 -0500

    bulkDelete report content type header

--- src/api/routes/bulkDelete.ts
+++ src/api/routes/bulkDelete.ts
@@ -8,7 +8,7 @@ export default class LinksRoute extends Route {
 
 		this.app
 			.get("/:id", async(req,res) => {
-				if (fs.existsSync(`${bulkDeleteDir}/${req.params.id}`)) return res.status(200).sendFile(`${bulkDeleteDir}/${req.params.id}`);
+				if (fs.existsSync(`${bulkDeleteDir}/${req.params.id}`)) return res.status(200).header("Content-Type", "text/plain").sendFile(`${bulkDeleteDir}/${req.params.id}`);
 				else return res.status(404).end("Unknown Report.");
 			});
 	}
