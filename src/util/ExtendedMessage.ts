e44da4ed6a2c9530c1223bc25b04a3dfe363cd4b
Donovan Daniels <hewwo@yiff.rocks>
2021-11-02 14:03:22 -0500

    eris:tm:

--- src/util/ExtendedMessage.ts
+++ src/util/ExtendedMessage.ts
@@ -39,9 +39,8 @@ export default class ExtendedMessage extends Message<Eris.GuildTextableChannel> 
 				// interaction message isn't a real message
 				if (typeof content !== "string" && content.messageReference) delete content.messageReference;
 				return self.cmdInteracton.createFollowup({
-					...(typeof content === "string" ? { content } : content),
-					file
-				}) as Promise<Eris.Message<Eris.TextChannel>>;
+					...(typeof content === "string" ? { content } : content)
+				}, file) as Promise<Eris.Message<Eris.TextChannel>>;
 				// eslint-disable-next-line deprecation/deprecation
 			} else return self.client.createMessage.call(self.client, this.id, content, file) as Promise<Eris.Message<Eris.TextChannel>>;
 		};
