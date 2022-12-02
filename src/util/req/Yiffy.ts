c9b6f7b2b5c0429b88bdfcd72c89beec5a03f72e
Donovan Daniels <hewwo@yiff.rocks>
2022-12-02 12:41:21 -0600

    bypass cloudflare

--- src/util/req/Yiffy.ts
+++ src/util/req/Yiffy.ts
@@ -1,11 +1,16 @@
-import Logger from "../Logger.js";
 import Config from "../../config/index.js";
 import YiffyAPI from "yiffy";
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 const Yiffy = new YiffyAPI({
-    userAgent: Config.userAgent,
-    apiKey:    Config.yiffyAPIKey,
-    debug:     (url, { time }) => Logger.getLogger("YiffyAPI").info(`API request for "${url}" took ${time}ms`)
+    userAgent:        Config.userAgent,
+    apiKey:           Config.yiffyAPIKey,
+    baseURL:          "https://yiff-rest.websites.containers.local",
+    host:             "v2.yiff.rest",
+    thumbsBaseURL:    "https://yiff-rest.websites.containers.local",
+    thumbsHost:       "thumbs.yiff.rest",
+    shortenerBaseURL: "https://yiff-rocks.websites.containers.local",
+    shortenerHost:    "yiff.rocks"
 });
 
 export default Yiffy;
