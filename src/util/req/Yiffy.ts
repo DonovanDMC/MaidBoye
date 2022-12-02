515ddfcc73d5877d34df714129c5a7084e77c656
Donovan Daniels <hewwo@yiff.rocks>
2022-12-02 12:48:55 -0600

    enable debug

--- src/util/req/Yiffy.ts
+++ src/util/req/Yiffy.ts
@@ -1,7 +1,9 @@
 import Config from "../../config/index.js";
 import YiffyAPI from "yiffy";
+import debug from "debug";
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+debug.enable("yiffy:*");
 const Yiffy = new YiffyAPI({
     userAgent:        Config.userAgent,
     apiKey:           Config.yiffyAPIKey,
