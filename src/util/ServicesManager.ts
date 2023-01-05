cb177e67f60b554e90b73ea517c566b29f6aa582
Donovan Daniels <hewwo@yiff.rocks>
2023-01-04 21:37:03 -0600

    fucking hell

--- src/util/ServicesManager.ts
+++ src/util/ServicesManager.ts
@@ -3,7 +3,6 @@ import Debug from "./Debug.js";
 import AutoPostingWebhookFailureHandler from "./handlers/AutoPostingWebhookFailureHandler.js";
 import MaidBoye from "../main.js";
 import Config from "../config/index.js";
-import AutoPostingEntry from "../db/Models/AutoPostingEntry.js";
 import { ChannelTypes } from "oceanic.js";
 import { randomUUID } from "node:crypto";
 import { Worker } from "node:worker_threads";
@@ -96,6 +95,7 @@ export default class ServicesManager {
                 }
 
                 case "AUTOPOST_FAILURE": {
+                    const AutoPostingEntry = (await import("../db/Models/AutoPostingEntry.js")).default;
                     const { bypassRequirements, entry: entryID } = data as { bypassRequirements: boolean; entry: string; };
                     const entry = await AutoPostingEntry.get(entryID);
                     if (!entry) {
