c486c814b46a6666f02c15ceb28fcc4f487c12c7
Donovan Daniels <hewwo@yiff.rocks>
2023-02-21 00:42:39 -0600

    add error logging for pastebin errors

--- src/util/handlers/ExceptionHandler.ts
+++ src/util/handlers/ExceptionHandler.ts
@@ -4,18 +4,22 @@ import type MaidBoye from "../../main.js";
 import Util from "../Util.js";
 import { Colors } from "../Constants.js";
 import Logger from "../Logger.js";
+import { Strings } from "@uwu-codes/utils";
 
 export default class ExceptionHandler {
     static client: MaidBoye;
-    static async createPaste(content: string, name: string, expire?: "N" | "10M" | "1H" | "1D" | "1W" | "2W" | "1M" | "6M" | "1Y", privacy?: 0 | 1 | 2) {
+    static async createPaste(content: string, name: string, expire: "N" | "10M" | "1H" | "1D" | "1W" | "2W" | "1M" | "6M" | "1Y" = "1W", privacy: 0 | 1 | 2 = 2) {
+        if (name.length > 100) {
+            name = Strings.truncate(name, 100);
+        }
         const form = new FormData();
         form.set("api_option", "paste");
         form.set("api_dev_key", Config.pastebinDevKey);
         form.set("api_user_key", Config.pastebinUserKey);
         form.set("api_paste_code", content);
         form.set("api_paste_name", name);
-        form.set("api_paste_expire_date", expire ?? "1W");
-        form.set("api_paste_private", privacy?.toString() ?? "2");
+        form.set("api_paste_expire_date", expire);
+        form.set("api_paste_private", privacy.toString());
         const res = await fetch("https://pastebin.com/api/api_post.php", {
             method: "POST",
             body:   form
@@ -23,7 +27,22 @@ export default class ExceptionHandler {
         if (res.status === 200) {
             return (await res.text()).replace("https://pastebin.com/", "");
         } else {
-            Logger.getLogger("Error Handler").error(`Pastebin API returned status code ${res.status} (${await res.text()})`);
+            const err = await res.text();
+            Logger.getLogger("Error Handler").error(`Pastebin API returned status code ${res.status} (${err})`);
+            await WebhookHandler.execute("error", {
+                embeds: Util.makeEmbed(true)
+                    .setTitle("Failed To Create Paste For Error")
+                    .setDescription([
+                        err,
+                        "",
+                        `Name: \`${name}\``,
+                        `Expire: \`${expire}\``,
+                        `Privacy: \`${privacy?.toString()}\``,
+                        `Content Length: \`${content.length}\``
+                    ])
+                    .setColor(Colors.red)
+                    .toJSON(true)
+            });
             return "null";
         }
     }
@@ -39,13 +58,13 @@ export default class ExceptionHandler {
         await WebhookHandler.execute("error", {
             embeds: Util.makeEmbed(true)
                 .setTitle(error.message)
-                .setDescription([
+                .setDescription(Strings.truncate([
                     `Source: **${Util.readableConstant(type.replace(/([A-Z])/g, "_$1"))}**`,
                     "",
                     `Code: \`${code}\``,
                     `Paste: ${Config.isDevelopment ? "**None**" : `https://pastebin.com/${paste}`}`,
                     context
-                ])
+                ].join("\n"), 2000))
                 .setColor(Colors.red)
                 .toJSON(true)
         });
