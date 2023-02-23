e2c91c016814b9b9eef8fe570bd152bbd0e509d6
Donovan Daniels <hewwo@yiff.rocks>
2023-02-23 05:08:11 -0600

    make use of guild specific avatars

--- src/interactions/applicationCommands/misc/avatar.ts
+++ src/interactions/applicationCommands/misc/avatar.ts
@@ -1,8 +1,9 @@
 import Command from "../../../util/cmd/Command.js";
 import Config from "../../../config/index.js";
 import Util from "../../../util/Util.js";
+import { UserCommand } from "../../../util/cmd/OtherCommand.js";
 import { ComponentBuilder } from "@oceanicjs/builders";
-import { ApplicationCommandOptionTypes, type MessageActionRow, MessageFlags } from "oceanic.js";
+import { ApplicationCommandOptionTypes, type MessageActionRow, MessageFlags, type User } from "oceanic.js";
 
 export default new Command(import.meta.url, "avatar")
     .setDescription("Get someone's avatar..")
@@ -25,11 +26,23 @@ export default new Command(import.meta.url, "avatar")
     })
     .setCooldown(3e3)
     .setExecutor(async function(interaction, { user }) {
+        const member = "guild" in interaction ? interaction.guild.members.get(user.id) : undefined;
         const c = new ComponentBuilder<MessageActionRow>();
-        c.addURLButton({
-            label: "Open Externally",
-            url:   user.avatarURL()
-        });
+        if (member) {
+            c.addURLButton({
+                label: "Open Server",
+                url:   member.avatarURL()
+            });
+            c.addURLButton({
+                label: "Open Global",
+                url:   user.avatarURL()
+            });
+        } else {
+            c.addURLButton({
+                label: "Open Externally",
+                url:   user.avatarURL()
+            });
+        }
         if (user.id === this.user.id) {
             c.addURLButton({
                 label: "Open Sauce",
@@ -37,12 +50,57 @@ export default new Command(import.meta.url, "avatar")
             });
         }
 
+        const e = Util.makeEmbed(true, interaction.user)
+            .setDescription(user.id === this.user.id ? `If you want to see the full version of my avatar, you can see it [here](${Config.botSauce}).` : (member ? "The user's global avatar is in the top right." : ""))
+            .setTitle(`Avatar of ${user.tag}`)
+            .setImage((member ?? user).avatarURL());
+        if (member) {
+            e.setThumbnail(user.avatarURL());
+        }
         return interaction.reply({
-            embeds: Util.makeEmbed(true, interaction.user)
-                .setDescription(user.id === this.user.id ? `If you want to see the full version of my avatar, you can see it [here](${Config.botSauce}).` : "")
-                .setTitle(`Avatar of ${user.tag}`)
-                .setImage(user.avatarURL())
+            embeds: e
                 .toJSON(true),
+            components: c.toJSON()
+        });
+    });
+
+export const userCommand = new UserCommand(import.meta.url, "avatar")
+    .setDescription("Get someone's avatar..")
+    .setAck(async function (interaction) {
+        const user = interaction.data.target as User;
+        const member = "guild" in interaction ? interaction.guild.members.get(user.id) : undefined;
+        const c = new ComponentBuilder<MessageActionRow>();
+        if (member) {
+            c.addURLButton({
+                label: "Open Server",
+                url:   member.avatarURL()
+            });
+            c.addURLButton({
+                label: "Open Global",
+                url:   user.avatarURL()
+            });
+        } else {
+            c.addURLButton({
+                label: "Open Externally",
+                url:   user.avatarURL()
+            });
+        }
+        if (user.id === this.user.id) {
+            c.addURLButton({
+                label: "Open Sauce",
+                url:   Config.botSauce
+            });
+        }
+
+        const e = Util.makeEmbed(true, interaction.user)
+            .setDescription(user.id === this.user.id ? `If you want to see the full version of my avatar, you can see it [here](${Config.botSauce}).` : (member ? "The user's global avatar is in the top right." : ""))
+            .setTitle(`Avatar of ${user.tag}`)
+            .setImage((member ?? user).avatarURL());
+        if (member) {
+            e.setThumbnail(user.avatarURL());
+        }
+        return interaction.reply({
+            embeds:     e.toJSON(true),
             components: c.toJSON()
         });
     });
