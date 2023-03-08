e2e513537e64e19068ca224e4faad9e7e842d2c2
Donovan Daniels <hewwo@yiff.rocks>
2023-03-08 13:00:24 -0600

    ignore e621 autocomplete errors

--- src/interactions/autocomplete/nsfw/E621Tags.ts
+++ src/interactions/autocomplete/nsfw/E621Tags.ts
@@ -2,6 +2,7 @@ import type { AnyAutocompleteFocus } from "../index.js";
 import type { AutocompleteInteraction } from "../../../util/cmd/Command.js";
 import E621 from "../../../util/req/E621.js";
 import BaseAutocomplete from "../structure/BaseAutocomplete.js";
+import { DiscordRESTError, JSONErrorCodes } from "oceanic.js";
 import assert from "node:assert";
 
 export default class E621TagsAutocomplete extends BaseAutocomplete {
@@ -12,25 +13,32 @@ export default class E621TagsAutocomplete extends BaseAutocomplete {
         assert(typeof focused.value === "string");
         const tags = focused.value.split(" ");
         const currentValue = tags[tags.length - 1];
-        if (!currentValue || currentValue.length < 3) {
-            return interaction.reply(focused.value === "" ? [
-                { name: "(NONE)", value: "" }
-            ] : [
-                { name: focused.value, value: focused.value }
-            ]);
-        } else {
-            const auto = await E621.tags.getAutocomplete(currentValue).catch(() => null);
-            if (auto === null) {
-                return interaction.reply([{ name: focused.value, value: focused.value }]);
+        try {
+            if (!currentValue || currentValue.length < 3) {
+                return interaction.reply(focused.value === "" ? [
+                    { name: "(NONE)", value: "" }
+                ] : [
+                    { name: focused.value, value: focused.value }
+                ]);
+            } else {
+                const auto = await E621.tags.getAutocomplete(currentValue).catch(() => null);
+                if (auto === null) {
+                    return interaction.reply([{ name: focused.value, value: focused.value }]);
+                }
+                const valueWithoutLast = focused.value.slice(0, focused.value.indexOf(currentValue)).trim();
+                return interaction.reply([
+                    { name: focused.value, value: focused.value },
+                    ...auto.map(a => ({
+                        name:  `${valueWithoutLast} ${a.name}`,
+                        value: `${valueWithoutLast} ${a.name}`
+                    }))
+                ]);
             }
-            const valueWithoutLast = focused.value.slice(0, focused.value.indexOf(currentValue)).trim();
-            return interaction.reply([
-                { name: focused.value, value: focused.value },
-                ...auto.map(a => ({
-                    name:  `${valueWithoutLast} ${a.name}`,
-                    value: `${valueWithoutLast} ${a.name}`
-                }))
-            ]);
+        } catch (err) {
+            if (err instanceof DiscordRESTError && err.code === JSONErrorCodes.UNKNOWN_INTERACTION) {
+                return;
+            }
+            throw err;
         }
     }
 }
