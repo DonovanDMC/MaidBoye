67194610ddbeca4fd806b40e96eb93d5943b4f17
Donovan Daniels <hewwo@yiff.rocks>
2023-03-18 21:29:54 -0500

    Gate settings behind permissions

--- src/interactions/applicationCommands/misc/settings.ts
+++ src/interactions/applicationCommands/misc/settings.ts
@@ -4,6 +4,7 @@ import Settings from "../../../util/settings/index.js";
 export default new Command(import.meta.url, "settings")
     .setDescription("Manage this server's settings.")
     .setOptionsGetter(() => Settings.getOptions())
+    .setPermissions("user", "MANAGE_GUILD")
     .setAck("ephemeral")
     .setValidLocation(ValidLocation.GUILD)
     .setGuildLookup(true)
