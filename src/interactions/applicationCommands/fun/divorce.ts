ef41a0ae3d2d4586214176d80d5d585a5695adfe
Donovan Daniels <hewwo@yiff.rocks>
2023-03-27 04:31:51 -0500

    make `user` required in divorce command

--- src/interactions/applicationCommands/fun/divorce.ts
+++ src/interactions/applicationCommands/fun/divorce.ts
@@ -10,15 +10,16 @@ export default new Command(import.meta.url, "divorce")
     .addOption(
         new Command.Option(ApplicationCommandOptionTypes.USER, "user")
             .setDescription("The user to divorce.")
+            .setRequired()
     )
     .setOptionsParser(interaction => ({
-        user: interaction.data.options.getUserOption("user", true)?.value
+        user: interaction.data.options.getUser("user", true)
     }))
     .setUserLookup(true)
     .setExecutor(async function(interaction, { user }, gConfig, uConfig) {
-        if (!uConfig.marriagePartners.includes(user)) {
+        if (!uConfig.marriagePartners.includes(user.id)) {
             return interaction.reply({
-                content: `H-hey! You aren't married to <@!${user}>...`
+                content: `H-hey! You aren't married to ${user.mention}...`
             });
         }
 
@@ -26,17 +27,17 @@ export default new Command(import.meta.url, "divorce")
         return interaction.reply({
             embeds: Util.makeEmbed(true, interaction.user)
                 .setTitle("Divorce")
-                .setDescription(`Are you sure you want to divorce <@!${user}>?`)
+                .setDescription(`Are you sure you want to divorce ${user.mention}?`)
                 .toJSON(true)
             ,
             components: new ComponentBuilder<MessageActionRow>()
                 .addInteractionButton({
-                    customID: State.new(interaction.user.id, "divorce", "yes").with("dUser", user).encode(),
+                    customID: State.new(interaction.user.id, "divorce", "yes").with("dUser", user.id).encode(),
                     label:    "Yes",
                     style:    ButtonColors.GREEN
                 })
                 .addInteractionButton({
-                    customID: State.new(interaction.user.id, "divorce", "no").with("dUser", user).encode(),
+                    customID: State.new(interaction.user.id, "divorce", "no").with("dUser", user.id).encode(),
                     label:    "No",
                     style:    ButtonColors.RED
                 })
