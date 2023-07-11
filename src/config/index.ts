077fe7de58f397f0e25d1427045b5cdb21a12023
Donovan Daniels <hewwo@yiff.rocks>
2023-07-10 21:54:48 -0500

    change the link again

--- src/config/index.ts
+++ src/config/index.ts
@@ -291,7 +291,7 @@ export class Configuration extends PrivateConfiguration {
 
     /* links */
     static get discordLink() {
-        return "https://discord.gg/XrzjzzbBD8";
+        return "https://discord.gg/8p5A5DVgXK";
     }
     static get webLink() {
         return "https://maidboye.cafe";
