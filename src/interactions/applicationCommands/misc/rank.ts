677a8449ea777a8ae381979c192e11ddba4c8cc9
Donovan Daniels <hewwo@yiff.rocks>
2024-06-03 14:57:53 -0500

    Add rank image & remove global rank

--- src/interactions/applicationCommands/misc/rank.ts
+++ src/interactions/applicationCommands/misc/rank.ts
@@ -3,6 +3,7 @@ import Leveling from "../../../util/Leveling.js";
 import Util from "../../../util/Util.js";
 import UserConfig from "../../../db/Models/UserConfig.js";
 import { ApplicationCommandOptionTypes } from "oceanic.js";
+import { profileImage } from "discord-arts";
 
 export default new Command(import.meta.url, "rank")
     .setDescription("Get a user's rank")
@@ -16,18 +17,33 @@ export default new Command(import.meta.url, "rank")
     .setValidLocation(ValidLocation.GUILD)
     .setExecutor(async function(interaction, { user }) {
         const { rank, total } = await Leveling.getUserRank(user.id, interaction.guildID);
-        const { rank: globalRank, total: globalTotal } = await Leveling.getUserRank(user.id, null);
         const xp = await UserConfig.getXP(user.id, interaction.guildID);
         const { level, leftover, needed } = Leveling.calcLevel(xp);
+        const img = await profileImage("242843345402069002", {
+            badgesFrame: true,
+            rankData:    {
+                currentXp:     leftover,
+                requiredXp:    leftover + needed,
+                rank,
+                level,
+                barColor:      "#A7A4AA",
+                autoColorRank: true
+            }
+        });
         return interaction.reply({
             embeds: Util.makeEmbed(true, interaction.user)
                 .setTitle(`Rank Info For ${user.tag}`)
                 .setDescription([
                     `Level: **${level}** (${leftover}/${leftover + needed})`,
                     `EXP: ${xp.toLocaleString()}`,
-                    `Local Rank: **${rank}**/**${total}**`,
-                    `Global Rank: **${globalRank}**/**${globalTotal}**`
+                    `Local Rank: **${rank}**/**${total}**`
                 ])
-                .toJSON(true)
+                .toJSON(true),
+            files: [
+                {
+                    name:     "rank.png",
+                    contents: img
+                }
+            ]
         });
     });
