39b3dbdd2a7e9de4100de6d0387e8b8d42de4410
Donovan Daniels <hewwo@yiff.rocks>
2024-06-05 07:53:35 -0500

    Make errors for discord-arts better

--- src/interactions/applicationCommands/misc/rank.ts
+++ src/interactions/applicationCommands/misc/rank.ts
@@ -8,7 +8,10 @@ import { profileImage } from "discord-arts";
 
 (global as unknown as { customBadges: Record<string, Array<string> | undefined>; }).customBadges = {
     "242843345402069002": ["https://cdn.discordapp.com/emojis/786675436243077140.png"],
-    "935392548506189884": ["https://cdn.discordapp.com/emojis/1213921250708168814.png"]
+    "935392548506189884": ["https://cdn.discordapp.com/emojis/1213921250708168814.png"],
+    "157988109445758976": ["https://cdn.discordapp.com/emojis/1016753364018663514.png"],
+    "339050872736579589": ["https://cdn.discordapp.com/emojis/994574107574276156.png"],
+    "738684490787979304": ["https://cdn.discordapp.com/emojis/1177533288861159434.png"]
 };
 export default new Command(import.meta.url, "rank")
     .setDescription("Get a user's rank")
